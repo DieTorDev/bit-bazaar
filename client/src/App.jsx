4a847265413cbf29c10f8c5c5fb3b0f7621affd3
diegotrazos <torradosanchezdiego@gmail.com>
2024-07-24 10:20:40 +0200

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,9 @@
-import Header from './components/Header/Header';
 import { GlobalStyles } from './styles/GlobalStyles';
 
 const App = () => {
 	return (
 		<>
 			<GlobalStyles />
-			<Header />
 		</>
 	);
 };
